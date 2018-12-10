b6c010c330731ad11ca35507eac50689f83bffd5
diZuluaga <zuluaga.rm.diego@gmail.com>
2018-12-09 20:43:57 -0500

    Updates

---
+++ precache-manifest.44300042c079eecfef2f014848f63398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76f31ce4f7af2cc134b3",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/static/css/main.d8ea16c2.chunk.css"
+  },
+  {
+    "revision": "76f31ce4f7af2cc134b3",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/static/js/main.76f31ce4.chunk.js"
+  },
+  {
+    "revision": "5f4cb496c3422b430b63",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/static/css/1.fe2d7b74.chunk.css"
+  },
+  {
+    "revision": "5f4cb496c3422b430b63",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/static/js/1.5f4cb496.chunk.js"
+  },
+  {
+    "revision": "9644f492a2f0e73c9f68",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/static/js/runtime~main.9644f492.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a4552d1dd86b61b578e99ae53ccfc3f3",
+    "url": "/MrDRMZ/simple-react-progressive-web-app.git/index.html"
+  }
+];
